25ca52092b8474dde470266271b8cfe6a2d1a0d6
Alessio Burgio <alessioburgio85@gmail.com>
2020-08-09 15:29:14 +0200

    update

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful';
 
 export default createClient({
-    space: mw7cne5r8g8w,
-    accessToken: HqatbcbBz5LjRhMINXoOdGJCdnUVW5SS3vSz6zfGvQ0
+    space: 'mw7cne5r8g8w',
+    accessToken: 'HqatbcbBz5LjRhMINXoOdGJCdnUVW5SS3vSz6zfGvQ0'
 });
