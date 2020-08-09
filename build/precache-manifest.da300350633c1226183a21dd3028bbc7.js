61babea537ef4ed69167c041313520739fa6073e
Alessio Burgio <alessioburgio85@gmail.com>
2020-08-09 14:03:50 +0200

    second commit

---
+++ build/precache-manifest.da300350633c1226183a21dd3028bbc7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3eca4be87aa8d50fc9f04fcc337f9f",
+    "url": "/resort_react/index.html"
+  },
+  {
+    "revision": "452f866f7d9c28e125c1",
+    "url": "/resort_react/static/css/main.0368bdc9.chunk.css"
+  },
+  {
+    "revision": "882e2577dbd998df678c",
+    "url": "/resort_react/static/js/2.82059e36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resort_react/static/js/2.82059e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452f866f7d9c28e125c1",
+    "url": "/resort_react/static/js/main.950d5de7.chunk.js"
+  },
+  {
+    "revision": "09ac9aa869700053d9d3",
+    "url": "/resort_react/static/js/runtime-main.67251339.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/resort_react/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/resort_react/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/resort_react/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/resort_react/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/resort_react/static/media/room-2.052c7681.jpeg"
+  }
+]);
